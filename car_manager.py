1c587becef37d1156cd948854796e3d9ec9dfc36
bheelz <53358143+ibheelz@users.noreply.github.com>
2024-01-14 20:08:10 +0100

    Added game over

--- car_manager.py
+++ car_manager.py
@@ -1,26 +1,27 @@
 import random
+from scoreboard import Scoreboard
 from turtle import Turtle
 
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
+
 class CarManager:
     def __init__(self):
-        self.cars = []
         self.speed = STARTING_MOVE_DISTANCE
-
+        self.cars = []
 
     def create_car(self):
-        self.car = Turtle()
-        self.car.penup()
-        self.car.shapesize(2, 4)
-        self.car.shape("square")
-        self.color = random.choice(COLORS)
-        self.car.color(self.color)
-        self.car.setheading(180)
-        self.car.setposition(790, random.randint(-400, 400))
-        self.cars.append(self.car)
+        new_car = Turtle()
+        new_car.penup()
+        new_car.shapesize(2, 4)
+        new_car.shape("square")
+        color = random.choice(COLORS)
+        new_car.color(color)
+        new_car.setheading(180)
+        new_car.setposition(790, random.randint(-400, 400))
+        self.cars.append(new_car)
 
     def move_car(self):
         for car in self.cars:
@@ -28,9 +29,3 @@ class CarManager:
 
     def car_speed(self):
         self.speed += MOVE_INCREMENT
-
-    def detect_collision(self, player):
-        for car in self.cars:
-            if player.player.distance(car) < 35:
-                return True
-        return False
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-import random
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
@@ -13,24 +12,28 @@ player = Player()
 car = CarManager()
 score = Scoreboard()
 
-
 count = 0
-
 game_is_on = True
+
 while game_is_on:
     count += 1
     time.sleep(0.1)
     screen.onkeypress(player.move, "Up")
+
     if count % 6 == 0:
         car.create_car()
+
     if player.is_at_finish_line():
         player.go_to_start()
         score.level_up()
         score.update_scoreboard()
         car.car_speed()
-    if car.detect_collision(player):
-        game_is_on = False
-        score.game_over()
+
+    for obstacle in car.cars:
+        if player.player.distance(obstacle) < 20:
+            score.game_over()
+            game_is_on = False
+
     car.move_car()
     screen.listen()
     screen.update()
